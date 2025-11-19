25a21be1ddae0735504017f49f37edc22afaa079
Олеся Замякина <tyuiu.zamjakinaoa@outlook.com>
2025-11-20 00:41:13 +0500

    task3

---
+++ Tyuiu.ZamyakinaOA.Sprint4.Task3.V10.Lib/Class1.cs
@@ -0,0 +1,30 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.ZamyakinaOA.Sprint4.Task3.V10.Lib
+{
+    public class Class1 : ISprint4Task3V10
+    {
+        public int Calculate(int[,] array)
+        {
+            int rows = array.GetUpperBound(0) + 1;
+            int colums = array.Length / rows;
+
+            int maxx = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < colums; j++)
+                {
+                    if (i == 3)
+                    {
+                        if (array[i, j] >  maxx)
+                        {
+                            maxx = array[i, j];
+                        }
+                    }
+                }
+            }
+            return maxx;
+        }
+    }
+}
---
+++ Tyuiu.ZamyakinaOA.Sprint4.Task3.V10.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.ZamyakinaOA.Sprint4.Task3.V10.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
