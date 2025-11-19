1fd20a7fe5a201108e27bb272211d6f12fa50d92
Олеся Замякина <tyuiu.zamjakinaoa@outlook.com>
2025-11-19 23:45:42 +0500

    task2

--- Tyuiu.ZamyakinaOA.Sprint4.Task2.V10.Lib/Class1.cs
+++ Tyuiu.ZamyakinaOA.Sprint4.Task2.V10.Lib/Class1.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.ZamyakinaOA.Sprint4.Task2.V10.Lib
     {
         public int Calculate(int[] array)
         {
-            int sum = 0;
+            int sum = 1;
             for (int i = 0; i <= array.Length - 1; i++)
             {
                 if (array[i] % 2 != 0)
