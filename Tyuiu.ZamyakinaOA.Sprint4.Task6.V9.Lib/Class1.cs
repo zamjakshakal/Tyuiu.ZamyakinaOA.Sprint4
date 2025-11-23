438e35ad1ca794beb8e697757429cc423f3badbf
Олеся Замякина <tyuiu.zamjakinaoa@outlook.com>
2025-11-23 17:36:23 +0500

    task6

---
+++ Tyuiu.ZamyakinaOA.Sprint4.Task6.V9.Lib/Class1.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.ZamyakinaOA.Sprint4.Task6.V9.Lib
+{
+    public class Class1 : ISprint4Task6V9
+    {
+        public int Calculate(string[] array)
+        {
+            string[] mas = Array.FindAll(array, x => x.Length < 7);
+            return mas.Length;
+        }
+    }
+}
---
+++ Tyuiu.ZamyakinaOA.Sprint4.Task6.V9.Test/Test1.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.ZamyakinaOA.Sprint4.Task6.V9.Lib;
+
+namespace Tyuiu.ZamyakinaOA.Sprint4.Task6.V9.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
